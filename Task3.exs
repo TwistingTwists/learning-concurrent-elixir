e03c202b6957edbf19c9cdbc9870a69137c3c2b0
Abhishek <abhishek.tripathi456@gmail.com>
2022-06-29 23:51:07 +0530

    Task.async_stream

---
+++ Task3.exs
@@ -0,0 +1,42 @@
+# HELPER FUNCTIONS
+defmodule CLI do
+  def add2(x) do
+    x + 2
+  end
+
+  def enum_task(range) do
+    range
+    |> Enum.map(fn x -> Task.async(fn -> CLI.add2(x) end) end)
+    |> Enum.map(&Task.await/1)
+  end
+
+  def enum_only(range) do
+    range
+    |> Enum.map(fn x -> CLI.add2(x) end)
+  end
+
+  def async_stream_task(range) do
+    range |> Task.async_stream(&CLI.add2/1) |> Enum.to_list()
+  end
+end
+
+# Stream
+Stream.map([1, 2, 3], fn x -> CLI.add2(x) end)
+Enum.map([1, 2, 3], fn x -> CLI.add2(x) end)
+Stream.map([1, 2, 3], fn x -> CLI.add2(x) end) |> Enum.to_list()
+
+# summary -> Stream is lazy Enum. (mostly)
+
+# PROBLEM of CONCURRENCY - SPIKES!!
+1..100 |> CLI.enum_task()
+
+# :observer.start()
+
+# ASYNC_STREAM
+1..100 |> CLI.async_stream_task()
+1..10000 |> CLI.async_stream_task()
+1..1_000_000 |> CLI.async_stream_task()
+
+1..1000_000 |> CLI.enum_only()
+1..1000_000 |> CLI.enum_task()
+1..1_000_000 |> CLI.async_stream_task()
