c01e66be11b8abe1177d3a9d58ea1ef4bc3ec9be
abhishek <abhishek.tripathi@tinymesh.in>
2023-03-27 21:12:43 +0530

    working icalendar rule with start date

--- icalendar.exs
+++ icalendar.exs
@@ -4,3 +4,5 @@ Mix.install([
 alias CalendarRecurrence.RRULE
 RRULE.parse("FREQ=DAILY;COUNT=10") |> IO.inspect(label: "minimal example")
 RRULE.to_recurrence("FREQ=WEEKLY;COUNT=4;BYDAY=MO,WE", ~D[2018-01-01]) |> Enum.to_list() |> IO.inspect(label: "recurrence rule")
+
+RRULE.to_recurrence("FREQ=DAILY;INTERVAL=1", ~D[2018-01-01]) |> Enum.take(10) |> IO.inspect(label: "recurrence rule real life")
