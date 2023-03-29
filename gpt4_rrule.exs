e10ebd6565c1c49b1b35a91b786419f49010cc28
abhishek <abhishek.tripathi@tinymesh.in>
2023-03-29 19:16:25 +0530

    more samples - membrance cmaf might not be a working example

--- gpt4_rrule.exs
+++ gpt4_rrule.exs
@@ -1,5 +1,6 @@
 Mix.install([
-  {:nimble_parsec, "~> 1.1"}
+  {:nimble_parsec, "~> 1.1"},
+  {:timex, "~> 3.0"}
 ])
 
 #
@@ -106,15 +107,25 @@ Mix.install([
 # DateTimeParser.parse("20220623T000000Z")
 # |> IO.inspect(label: "dateimt parser ")
 
+defmodule Helpers do
+  def pad_leading(x) do
+    case String.length(x) do
+      4 -> x
+      n when n < 2 -> String.pad_leading(x, 2, "0")
+    end
+  end
+end
+
 defmodule RRuleParser do
   import NimbleParsec
+  import Helpers
 
   # Define parsers for different parts of the RRULE
-  rrule_parser = string("RRULE")
+  rrule_parser = ignore(string("RRULE"))
 
   freq_parser =
     string("FREQ")
-    |> string("=")
+    |> ignore(string("="))
     |> choice([
       string("SECONDLY"),
       string("MINUTELY"),
@@ -128,8 +139,8 @@ defmodule RRuleParser do
   integer_parser = integer(min: 1)
 
   dtstart_parser =
-    string("DTSTART")
-    |> string(":")
+    ignore(string("DTSTART"))
+    |> ignore(string(":"))
     |> integer(4)
     |> integer(2)
     |> integer(2)
@@ -138,24 +149,25 @@ defmodule RRuleParser do
     |> integer(2)
     |> integer(2)
     |> string("Z")
+    |> reduce({Enum, :map_join, ["", &pad_leading/1]})
 
   # |> eos()
 
   # choice([string("DATE-TIME"), string("DATE"), string("PERIOD")])
 
-  interval_parser = string("INTERVAL") |> string("=") |> integer(min: 1)
+  interval_parser = ignore(string("INTERVAL")) |> ignore(string("=")) |> integer(min: 1)
 
-  count_parser = string("COUNT") |> integer(min: 1)
+  count_parser = ignore(string("COUNT")) |> integer(min: 1)
 
-  until_parser = string("UNTIL") |> choice([string("DATE-TIME"), string("DATE")])
+  until_parser = ignore(string("UNTIL")) |> choice([string("DATE-TIME"), string("DATE")])
 
   # Combine all parsers
   rrule_parser =
     dtstart_parser
-    |> optional(concat(string(";"), rrule_parser))
-    |> optional(concat(string(":"), freq_parser))
-    |> optional(concat(string(";"), interval_parser))
-    # |> optional(concat(string(";"), count_parser))
+    |> optional(concat(ignore(string(";")), rrule_parser))
+    |> optional(concat(ignore(string(":")), freq_parser))
+    |> optional(concat(ignore(string(";")), interval_parser))
+    |> optional(concat(ignore(string(";")), count_parser))
     # |> optional(concat(string(";"), until_parser))
     |> optional(string(";"))
     |> eos()
@@ -163,7 +175,85 @@ defmodule RRuleParser do
   defparsec(:parse, rrule_parser, debug: true)
 end
 
+# this works.
 rrule = "DTSTART:20220623T000000Z;RRULE:FREQ=DAILY;INTERVAL=1"
-{:ok, result, _, _} = RRuleParser.parse(rrule)
+{:ok, result, _, _, _, _} = RRuleParser.parse(rrule)
 # Check the
-IO.inspect(result)
+IO.inspect(result, label: "only rrule")
+
+defmodule RRuleProcessor do
+  @moduledoc """
+  A basic RRule processor that calculates the next occurrence based on the parsed RRULE.
+  """
+
+  # For simplicity, let's assume the parsed RRULE contains only the required fields.
+  def next_occurrence(dtstart, freq, interval \\ 1) do
+    case freq do
+      "SECONDLY" ->
+        dtstart |> add_seconds(interval)
+
+      "MINUTELY" ->
+        dtstart |> add_minutes(interval)
+
+      "HOURLY" ->
+        dtstart |> add_hours(interval)
+
+      "DAILY" ->
+        dtstart |> add_days(interval)
+
+      "WEEKLY" ->
+        dtstart |> add_weeks(interval)
+
+      "MONTHLY" ->
+        dtstart |> add_months(interval)
+
+      "YEARLY" ->
+        dtstart |> add_years(interval)
+
+      _ ->
+        {:error, "Invalid frequency"}
+    end
+  end
+
+  # Helper functions to add time intervals
+  defp add_seconds(datetime, seconds) do
+    datetime |> Timex.shift(seconds: seconds)
+  end
+
+  defp add_minutes(datetime, minutes) do
+    datetime |> Timex.shift(minutes: minutes)
+  end
+
+  defp add_hours(datetime, hours) do
+    datetime |> Timex.shift(hours: hours)
+  end
+
+  defp add_days(datetime, days) do
+    datetime |> Timex.shift(days: days)
+  end
+
+  defp add_weeks(datetime, weeks) do
+    datetime |> Timex.shift(weeks: weeks)
+  end
+
+  defp add_months(datetime, months) do
+    datetime |> Timex.shift(months: months)
+  end
+
+  defp add_years(datetime, years) do
+    datetime |> Timex.shift(years: years)
+  end
+end
+
+rrule2 = "DTSTART:20220101T000000Z;RRULE:FREQ=DAILY;INTERVAL=2"
+{:ok, result, _, _, _, _} = RRuleParser.parse(rrule2) |> IO.inspect(label: "RRULE2")
+# [year, month, day, hr, minute, second, "FREQ", freq, "INTERVAL", interval] = result
+parsed_date = hd(result)
+
+dtstart =
+  Timex.parse!(parsed_date, "{ISO:Basic:Z}")
+  |> IO.inspect(label: "parsed date from timex")
+
+# next_event =
+#   RRuleProcessor.next_occurrence(dtstart, parsed_freq, parsed_interval)
+#   |> IO.inspect(label: "next events")
---
+++ membrance_cmaf.exs
@@ -0,0 +1,88 @@
+Mix.install([
+  :membrane_aac_plugin,
+  :membrane_h264_ffmpeg_plugin,
+  :membrane_hackney_plugin,
+  :membrane_http_adaptive_stream_plugin,
+  :membrane_mp4_plugin
+])
+
+defmodule Example do
+  use Membrane.Pipeline
+
+  alias Membrane.Time
+  alias Membrane.MP4.Muxer.CMAF.SegmentDurationRange
+  alias Membrane.HTTPAdaptiveStream.Sink, as: HLSSink
+
+  @samples_url "https://raw.githubusercontent.com/membraneframework/static/gh-pages/samples/big-buck-bunny/"
+  @audio_url @samples_url <> "bun33s.aac"
+  @video_url @samples_url <> "bun33s_720x480.h264"
+  @output_dir "hls_output"
+
+  @impl true
+  def handle_init(_ctx, _opts) do
+    File.rm_rf(@output_dir)
+    File.mkdir!(@output_dir)
+
+    structure = [
+      child(:video_source, %Membrane.Hackney.Source{
+        location: @video_url,
+        hackney_opts: [follow_redirect: true]
+      })
+      |> child(:video_parser, %Membrane.H264.FFmpeg.Parser{
+        framerate: {25, 1},
+        alignment: :au,
+        attach_nalus?: true
+      })
+      |> child(:video_payloader, Membrane.MP4.Payloader.H264)
+      |> via_in(Pad.ref(:input, :video))
+      |> get_child(:muxer),
+      child(:audio_source, %Membrane.Hackney.Source{
+        location: @audio_url,
+        hackney_opts: [follow_redirect: true]
+      })
+      |> child(:audio_parser, %Membrane.AAC.Parser{
+        in_encapsulation: :ADTS,
+        out_encapsulation: :none
+      })
+      |> child(:audio_payloader, Membrane.MP4.Payloader.AAC)
+      |> via_in(Pad.ref(:input, :audio))
+      |> get_child(:muxer),
+      child(:muxer, %Membrane.MP4.Muxer.CMAF{
+        segment_duration_range: SegmentDurationRange.new(Time.seconds(4), Time.seconds(6))
+      })
+      |> via_in(:input, options: [segment_duration: HLSSink.SegmentDuration.new(Time.seconds(12))])
+      |> child(:sink, %HLSSink{
+        manifest_module: Membrane.HTTPAdaptiveStream.HLS,
+        target_window_duration: Membrane.Time.seconds(30),
+        persist?: true,
+        storage: %Membrane.HTTPAdaptiveStream.Storages.FileStorage{
+          directory: @output_dir
+        }
+      })
+    ]
+
+    {[spec: structure, playback: :playing], %{}}
+  end
+
+  # The rest of the module is used only for pipeline self-termination after processing finishes
+  @impl true
+  def handle_element_end_of_stream(:sink, _pad, _ctx, state) do
+    {[terminate: :shutdown], state}
+  end
+
+  @impl true
+  def handle_element_end_of_stream(_child, _pad, _ctx, state) do
+    {[], state}
+  end
+end
+
+# Start and monitor the pipeline
+{:ok, _supervisor_pid, pipeline_pid} = Example.start_link()
+ref = Process.monitor(pipeline_pid)
+
+# Wait for the pipeline to finish
+receive do
+  {:DOWN, ^ref, :process, _pipeline_pid, _reason} ->
+    :ok
+end
+
