cac5a8f0b8c1791db36b5004628ce81ce28c0533
abhishek <abhishek.tripathi456@gmail.com>
2023-08-25 21:26:53 +0530

    basic task with a receive loop.
    can be used to call callback function.

--- awaiting_on_task.exs
+++ awaiting_on_task.exs
@@ -3,17 +3,34 @@
 defmodule CLI do
   def run() do
     Task.async(fn ->
+      self()
+      |> IO.inspect(label: "task is running in self(): ")
+
       Process.sleep(1000)
       IO.inspect("Slept for 1 minute")
-      5
+
+      receive do
+        {:ok, num} ->
+          IO.inspect(num, label: "received a num")
+          num + 10
+
+        anything_else ->
+          IO.inspect(anything_else, label: "received anything else")
+      end
     end)
   end
 end
 
 defmodule CallerProcess do
-  CLI.run()
-  |> Task.await()
+  task =
+    CLI.run()
+    |> IO.inspect(label: "task")
+
+  send(task.pid, {:ok, 666})
+  |> IO.inspect(label: "send")
+
+  task |> Task.await() |> IO.inspect(label: "task output ")
 
   self() |> IO.inspect(label: "pid")
-  Process.sleep(6000)
+  Process.sleep(60000)
 end
