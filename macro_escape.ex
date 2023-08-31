e2117306b64b283753226c965638af9b80a19568
abhishek <abhishek.tripathi456@gmail.com>
2023-08-31 15:31:37 +0530

    if `val` is not quoted  {:val, [] Elixir} is the AST, not 1 (the value of `val`) - thats why the warning says `val` is not used.

--- macro_escape.ex
+++ macro_escape.ex
@@ -6,13 +6,18 @@ defmodule FooBar do
     def unquote(f)(unquote(val)), do: true
     def unquote(f)(_), do: false
   end
-  |> IO.inspect(label: "for loop")
+
+  # for {f, val} <- Enum.zip(@fname, @fvalues) do
+  #   def unquote(f)(val), do: true
+  #   def unquote(f)(_), do: false
+  # end
+  # {:val, [] Elixir} is the AST, not 1 (the value of `val`) - thats why the warning says `val` is not used.
 end
 
-FooBar.is_one(1)
-|> IO.inspect(label: "is_one")
+# FooBar.is_one(1)
+# |> IO.inspect(label: "is_one")
 
-FooBar.is_two(4)
-|> IO.inspect(label: "is_two")
+# FooBar.is_two(4)
+# |> IO.inspect(label: "is_two")
 
-# FooBar.__info__(:functions) |> IO.inspect(label: "functions")
+FooBar.__info__(:functions) |> IO.inspect(label: "functions")
