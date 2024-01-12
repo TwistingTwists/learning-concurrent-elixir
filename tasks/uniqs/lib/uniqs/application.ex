f890277eefc338a03a93f112e716473e1ccf2c9c
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-01-12 15:11:48 +0530

    unique_task - run it for uniquely running tasks

--- tasks/uniqs/lib/uniqs/application.ex
+++ tasks/uniqs/lib/uniqs/application.ex
@@ -5,12 +5,9 @@ defmodule Uniqs.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Uniqs.Worker.start_link(arg)
-      # {Uniqs.Worker, arg}
+      Livebook.Utils.UniqueTask
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Uniqs.Supervisor]
     Supervisor.start_link(children, opts)
   end
